0d8ce52b33bc3d1e57669dc6e0271ec74e8a927a
Timofey Tsvirko <meriscan.ru@gmail.com>
2024-10-13 05:27:17 +0300

    [develop] [NF]: added metrics

--- scripts/process_folder.py
+++ scripts/process_folder.py
@@ -1,9 +1,13 @@
 import json
+import os
+import time
 from datetime import datetime
 from pathlib import Path
 
 import click
+import numpy as np
 import pandas as pd
+import psutil
 from tqdm import tqdm
 
 from szfo_2024_solution.voice2text import MetricsCalculator, VoskASR
@@ -34,8 +38,17 @@ def greet(voices_dpath, voice_ext, frame_rate, annotation_dpath):
     model = VoskASR(MODELS_DPATH / "vosk-model-small-ru-0.22", frame_rate=frame_rate)
 
     recognized_data = []
+
+    # latency and peak RAM usage
+    latency_list = []
+    process = psutil.Process(os.getpid())
+    peak_memory = process.memory_info().rss
+
     for voice_dpath in tqdm(list(voices_dpath.glob(f"*.{voice_ext}"))):
         model.read_audio(voice_dpath)
+
+        start_time = time.time()
+
         model.denoise_audio()
         recognized_text = model.recognize_audio()
         pred_id = model.get_pred_label
@@ -48,10 +61,20 @@ def greet(voices_dpath, voice_ext, frame_rate, annotation_dpath):
                 pred_attribute,
             ]
         )
+
+        end_time = time.time()
+        latency = end_time - start_time
+        latency_list.append(latency)
+
+        peak_memory = max(peak_memory, process.memory_info().rss)
+
     recognized_df = pd.DataFrame(
         recognized_data,
         columns=["audio_filepath", "recognized_text", "pred_label", "pred_attribute"],
     )
+
+    print("Latency:", np.mean(latency_list))
+    print("Peak RAM:", peak_memory / (1024 * 1024), "MB")
 
     if annotations_df is not None:
         recognized_df = pd.merge(recognized_df, annotations_df, on="audio_filepath")
@@ -62,6 +85,13 @@ def greet(voices_dpath, voice_ext, frame_rate, annotation_dpath):
             ),
             axis=1,
         )
+        print("------------------")
+        print("WER:", recognized_df["WER"].mean())
+        f1_weighted = MetricsCalculator.f1_w(
+            recognized_df["label"],
+            recognized_df["pred_label"],
+        )
+        print("F1-weighted:", f1_weighted)
 
     output_path = DATA_DPATH / "results"
     output_path.mkdir(parents=True, exist_ok=True)
